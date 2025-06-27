b777289045a021faa04419aa389f248e4ede6746
Shiran <127624730+Shiranakther@users.noreply.github.com>
2025-06-28 03:34:24 +0530

    Add Category component and integrate it into Home page; update styles and layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home';
-
+import Category from '../components/Category';
 
 function App() {
 
@@ -11,6 +11,8 @@ function App() {
    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cat" element={<Category />} />
+        
         
       </Routes>
     </Router>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Landing from '../../components/Landing'
+import Category from '../../components/Category'
+import { MdCake } from 'react-icons/md';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Landing />
-      <p>Hello shinu</p>
+      
+      <div className='flex flex-col justify-center items-center'  style={{ background: '#fff7ed' }}>
+        <MdCake size={45} style={{ color: 'var(--color-roce-100)', }} className='mt-10' />
+        <div className='mt-5 text-4xl font-bold text-gray-700  '  >Sweet Categories</div>
+        <div className='text-2xl  text-gray-700 mt-4'>Explore our delicious cake categories</div>
+      </div>
+
+      
+      
+     <div className='flex flex-row justify-evenly items-center pt-12 mb-10 flex-wrap gap-10 pb-12 ' 
+     style={{ background: '#fff7ed' }}
+     >
+       {Array.from({ length: 8 }).map((_, i) => (
+          <Category key={i} />
+        ))}
+      
+    </div>
     </div>
   )
 }
